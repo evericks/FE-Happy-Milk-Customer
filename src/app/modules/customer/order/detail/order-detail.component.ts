e51c6f6b53ceb9eb5184f57efe366d52853109e5
Evericks <votantai4899@gmail.com>
2024-07-07 04:08:02 +0700

    UI for order detail

--- src/app/modules/customer/order/detail/order-detail.component.ts
+++ src/app/modules/customer/order/detail/order-detail.component.ts
@@ -5,12 +5,13 @@ import { OrderService } from '../order.service';
 import { CommonModule } from '@angular/common';
 import { CustomPipesModule } from '@fuse/pipes/custom/custom-pipes.module';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
     selector: 'order-detail',
     standalone: true,
     templateUrl: 'order-detail.component.html',
-    imports: [CommonModule, CustomPipesModule, MatIconModule]
+    imports: [CommonModule, CustomPipesModule, MatIconModule, MatButtonModule]
 })
 
 export class OrderDetailComponent implements OnInit {
